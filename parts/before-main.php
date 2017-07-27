7f63c4b147051c1e848573c82cd9096d5b5e7fd7
Phil Cable <pcable@wsu.edu>
2017-07-27 10:24:58 -0700

    Remove global bar header element

--- parts/before-main.php
+++ parts/before-main.php
@@ -11,11 +11,6 @@ $murrow_global_top_menu_args = array(
 );
 
 ?>
-	<div class="global-bar-wrap">
-		<div class="global-bar">
-			<a class="global-home" href="https://wsu.edu">Washington State University</a>
-		</div>
-	</div>
 	<div id="contact-search" class="site-search">
 		<?php wp_nav_menu( $murrow_global_top_menu_args ); ?>
 	</div>
