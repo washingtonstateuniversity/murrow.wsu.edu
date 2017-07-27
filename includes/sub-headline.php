c7bd93c60fc39bffe2d3c100ea855232fe6f17d8
Jeremy Felt <jeremy.felt@gmail.com>
2017-07-27 11:57:26 -0700

    Move existing sub headline code into new namespaced file

--- includes/sub-headline.php
+++ includes/sub-headline.php
@@ -4,6 +4,59 @@ namespace WSU\Murrow\Sub_Headline;
 
 add_action( 'rest_api_init', 'WSU\Murrow\Sub_Headline\register_api_field' );
 add_filter( 'wsu_content_syndicate_host_data', 'WSU\Murrow\Sub_Headline\modify_content_syndicate_data', 10, 3 );
+add_action( 'edit_form_after_title', 'WSU\Murrow\Sub_Headline\subhead_input' );
+add_action( 'save_post', 'WSU\Murrow\Sub_Headline\save_post', 10, 2 );
+
+/**
+ * Adds an input for capturing a subhead below the post title field.
+ *
+ * @param \WP_Post $post Post object.
+ */
+function subhead_input( $post ) {
+	if ( 'post' !== $post->post_type ) {
+		return;
+	}
+
+	wp_nonce_field( 'save-murrow-subhead', '_murrow_subhead_nonce' );
+
+	$subhead = get_post_meta( $post->ID, '_murrow_subhead', true );
+	$subhead_editor_settings = array(
+		'media_buttons' => false,
+		'textarea_rows' => 2,
+		'teeny' => true,
+		'quicktags' => false,
+	);
+
+	?><h2>Subhead</h2><?php
+
+	wp_editor( $subhead, '_murrow_subhead', $subhead_editor_settings );
+}
+
+/**
+ * Saves additional data associated with a post.
+ *
+ * @param int     $post_id
+ * @param \WP_Post $post
+ */
+function save_post( $post_id, $post ) {
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+		return;
+	}
+
+	if ( 'auto-draft' === $post->post_status ) {
+		return;
+	}
+
+	if ( ! isset( $_POST['_murrow_subhead_nonce'] ) || ! wp_verify_nonce( $_POST['_murrow_subhead_nonce'], 'save-murrow-subhead' ) ) {
+		return;
+	}
+
+	if ( isset( $_POST['_murrow_subhead'] ) ) {
+		update_post_meta( $post_id, '_murrow_subhead', wp_kses_post( $_POST['_murrow_subhead'] ) );
+	} else {
+		delete_post_meta( $post_id, '_murrow_subhead' );
+	}
+}
 
 /**
  * Register an additional REST API field for sub headlines.
