5435c564ab1497e3b1b713faf5acdbd5aa0e72a0
Jeremy Felt <jeremy.felt@gmail.com>
2017-07-27 16:37:50 -0700

    Bump version 0.0.4

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@ include_once __DIR__ . '/includes/people-directory.php';
 
 add_filter( 'spine_child_theme_version', 'murrow_theme_version' );
 function murrow_theme_version() {
-	return '0.0.3';
+	return '0.0.4';
 }
 
 add_action( 'init', 'murrow_remove_spine_wp_enqueue_scripts' );
