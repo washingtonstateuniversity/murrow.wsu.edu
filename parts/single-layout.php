3486cd52d1471e2b78419cfdf40e55102dc83e37
Jeremy Felt <jeremy.felt@gmail.com>
2017-08-01 16:15:28 -0700

    Add a custom single-layout.php template file
    Change to `single` from `side-right` and remove `pad-ends`

---
+++ parts/single-layout.php
@@ -0,0 +1,13 @@
+<section class="row single gutter">
+
+	<div class="column one">
+
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<?php get_template_part( 'articles/post', get_post_type() ) ?>
+
+		<?php endwhile; ?>
+
+	</div><!--/column-->
+
+</section>
