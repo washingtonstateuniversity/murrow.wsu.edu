6520d90adfba4743b896ff2ca8630b20493360fd
Jeremy Felt <jeremy.felt@gmail.com>
2017-08-02 14:53:33 -0700

    Remove featured images output from search template
    This could be re-added, but unless we plan for it should not be
    part of the display

--- templates/search.php
+++ templates/search.php
@@ -11,7 +11,6 @@ get_header();
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 			<?php get_template_part( 'parts/headers' ); ?>
-			<?php get_template_part( 'parts/featured-images' ); ?>
 
 			<section class="row single gutter pad-ends">
 				<div class="column one">
