229b2343dcf788cde3da8eb981acbf37568302fd
Jeremy Felt <jeremy.felt@gmail.com>
2017-08-08 14:12:19 -0700

    Do not display breadcrumbs on home page
    Fixes #78

--- parts/headers.php
+++ parts/headers.php
@@ -1,6 +1,6 @@
 <?php
 
-if ( spine_display_breadcrumbs( 'top' ) ) {
+if ( ! is_front_page() && spine_display_breadcrumbs( 'top' ) ) {
 	$breadcrumb_display = apply_filters( 'murrow_filter_breadcrumb', bcn_display( true ) );
 	?>
 	<section class="row single breadcrumbs breadcrumbs-top gutter pad-top" typeof="BreadcrumbList" vocab="http://schema.org/">
