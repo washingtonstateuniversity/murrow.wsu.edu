52b28dbb65a8cf34d417dff8109fa8474f3a323c
Jeremy Felt <jeremy.felt@gmail.com>
2017-08-09 10:19:00 -0700

    Bump version 0.4.1

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@ include_once __DIR__ . '/includes/breadcrumb.php';
 
 add_filter( 'spine_child_theme_version', 'murrow_theme_version' );
 function murrow_theme_version() {
-	return '0.4.0';
+	return '0.4.1';
 }
 
 // Disable background image selection for posts and pages.
