729fc78657d2cb6d0f433c873286a2fa9d90e60b
Jeremy Felt <jeremy.felt@gmail.com>
2017-09-11 14:41:38 -0700

    Add the theme's version number to its editor stylesheet

--- includes/editor.php
+++ includes/editor.php
@@ -4,6 +4,15 @@ namespace WSU\Murrow\Editor;
 
 add_filter( 'mce_buttons', 'WSU\Murrow\Editor\add_styleselect_button' );
 add_filter( 'tiny_mce_before_init', 'WSU\Murrow\Editor\custom_style_formats' );
+add_action( 'admin_init', 'WSU\Murrow\Editor\add_editor_style', 20 );
+add_filter( 'editor_stylesheets', 'WSU\Murrow\Editor\editor_stylesheets_version' );
+
+/**
+ * Add a stylesheet to TinyMCE to help format custom content.
+ */
+function add_editor_style() {
+	\add_editor_style( 'editor/editor-style.css' );
+}
 
 /**
  * Add the "Format" button as the second option in the top row of TinyMCE.
@@ -40,4 +49,23 @@ function custom_style_formats( $settings ) {
 	$settings['style_formats'] = wp_json_encode( $style_formats );
 
 	return $settings;
+}
+
+/**
+ * Add a version number to the theme's editor stylesheets.
+ *
+ * @param array $stylesheets
+ *
+ * @return array
+ */
+function editor_stylesheets_version( $stylesheets ) {
+	foreach( $stylesheets as $k => $stylesheet ) {
+		if ( 0 !== strpos( $stylesheet, get_stylesheet_directory_uri() ) ) {
+			continue;
+		}
+
+		$stylesheets[ $k ] = $stylesheet . '?v=' . spine_get_child_version();
+	}
+
+	return $stylesheets;
 }
